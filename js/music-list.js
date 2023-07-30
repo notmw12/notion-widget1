8d33e1fc4f6c8d36821387beeb9d457f1b4aacb9
notmw12 <140984129+notmw12@users.noreply.github.com>
2023-07-31 01:14:00 +0300

    Adding files

---
+++ js/music-list.js
@@ -0,0 +1,326 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Anti-Romantic",
+    artist: "TOMORROW X TOGETHER",
+    img: "./images/01.jpg",
+    src: "01"
+  },
+  {
+    name: "24",
+    artist: "JEM",
+    img: "./images/02.jpg",
+    src: "02"
+  },
+  {
+    name: "Candy",
+    artist: "Doja Cat",
+    img: "./images/03.jpg",
+    src: "03"
+  },
+  {
+    name: "Colide (feat.Tyga)",
+    artist: "Justine Sky",
+    img: "./images/04.jpg",
+    src: "04"
+  },
+  {
+    name: "Con altura (feat. El Guincho)",
+    artist: "J Balvin & ROSALíA",
+    img: "./images/05.jpg",
+    src: "05"
+  },
+  {
+    name: "Crying in the club",
+    artist: "Camila Cabello",
+    img: "./images/06.jpg",
+    src: "06"
+  },
+  {
+    name: "Dangerous women",
+    artist: "Ariana Grande",
+    img: "./images/07.jpg",
+    src: "07"
+  },
+  {
+    name: "Despetcha",
+    artist: "ROSALíA",
+    img: "./images/08.jpg",
+    src: "08"
+  },
+  {
+    name: "Die for you remix",
+    artist: "The Weekend & Ariana Grande",
+    img: "./images/09.jpg",
+    src: "09"
+  },
+  {
+    name: "Escapisim spedup",
+    artist: "RAYE & 070 Shake",
+    img: "./images/10.jpg",
+    src: "10"
+  },
+  {
+    name: "Escapisim",
+    artist: "RAYE & 070 Shake",
+    img: "./images/11.jpg",
+    src: "11"
+  },
+  {
+    name: "Fever",
+    artist: "Dua lipa & Angèle",
+    img: "./images/12.jpg",
+    src: "12"
+  },
+  {
+    name: "Get into it (Yuh)",
+    artist: "Doja Cat",
+    img: "./images/13.jpg",
+    src: "13"
+  },
+  {
+    name: "Harleys in hawaii",
+    artist: "Katy Perry",
+    img: "./images/14.jpg",
+    src: "14"
+  },
+  {
+    name: "Hey, Mickey!",
+    artist: "Baby Tate",
+    img: "./images/15.jpg",
+    src: "15"
+  },
+  {
+    name: "16 shots",
+    artist: "Stefflon Don",
+    img: "./images/16.jpg",
+    src: "16"
+  },
+  {
+    name: "Kill bill",
+    artist: "SZA",
+    img: "./images/17.jpg",
+    src: "17"
+  },
+  {
+    name: "Kiss me more",
+    artist: "Doja Cat & SZA",
+    img: "./images/18.jpg",
+    src: "18"
+  },
+  {
+    name: "Last hurrah",
+    artist: "Bebe Rexha",
+    img: "./images/19.jpg",
+    src: "19"
+  },
+  {
+    name: "Lost on you",
+    artist: "LP",
+    img: "./images/20.jpg",
+    src: "20"
+  },
+  {
+    name: "Love you like me",
+    artist: "William Singe",
+    img: "./images/21.jpg",
+    src: "21"
+  },
+  {
+    name: "Low",
+    artist: "SZA",
+    img: "./images/22.jpg",
+    src: "22"
+  },
+  {
+    name: "Man down",
+    artist: "Rihanna",
+    img: "./images/23.jpg",
+    src: "23"
+  },
+  {
+    name: "BBH",
+    artist: "Billie Eilish",
+    img: "./images/24.jpg",
+    src: "24"
+  },
+  {
+    name: "Mmmh",
+    artist: "Kai",
+    img: "./images/25.jpg",
+    src: "25"
+  },
+  {
+    name: "My oh my",
+    artist: "Camila Cabello",
+    img: "./images/26.jpg",
+    src: "26"
+  },
+  {
+    name: "No guidance",
+    artist: "Chris Brown & Drake",
+    img: "./images/27.jpg",
+    src: "27"
+  },
+  {
+    name: "One more night",
+    artist: "Maroon 5",
+    img: "./images/28.jpg",
+    src: "28"
+  },
+  {
+    name: "One of The girls",
+    artist: "The Weekend, JENNIE & Lily depp",
+    img: "./images/29.jpg",
+    src: "29"
+  },
+  {
+    name: "Papi",
+    artist: "Yarin Primak",
+    img: "./images/30.jpg",
+    src: "30"
+  },
+  {
+    name: "Play with fire (feat. Yacht money)",
+    artist: "Sam Tinnesz",
+    img: "./images/31.jpg",
+    src: "31"
+  },
+  {
+    name: "Price tag",
+    artist: "Jessie J",
+    img: "./images/32.jpg",
+    src: "32"
+  },
+  {
+    name: "Red lights",
+    artist: "Bang chan & Hyunjin of SKZ",
+    img: "./images/33.jpg",
+    src: "33"
+  },
+  {
+    name: "Renegade (sped up)",
+    artist: "WulfBlo0d",
+    img: "./images/34.jpg",
+    src: "34"
+  },
+  {
+    name: "Shameless",
+    artist: "Camila Cabello",
+    img: "./images/35.jpg",
+    src: "35"
+  },
+  {
+    name: "Shirt",
+    artist: "SZA",
+    img: "./images/36.jpg",
+    src: "36"
+  },
+  {
+    name: "Shoong (feat.Lisa)",
+    artist: "Teayang",
+    img: "./images/37.jpg",
+    src: "37"
+  },
+  {
+    name: "Slow motion",
+    artist: "Amaria BB",
+    img: "./images/38.jpg",
+    src: "38"
+  },
+  {
+    name: "Slumber party (Princess Nokia)",
+    artist: "Ashinkko",
+    img: "./images/39.jpg",
+    src: "39"
+  },
+  {
+    name: "Swalla",
+    artist: "Jason Derulo & Nicky minaj",
+    img: "./images/40.jpg",
+    src: "40"
+  },
+  {
+    name: "Sweet but psycho",
+    artist: "Ava Max",
+    img: "./images/41.jpg",
+    src: "41"
+  },
+  {
+    name: "Big boy",
+    artist: "SZA",
+    img: "./images/42.jpg",
+    src: "42"
+  },
+  {
+    name: "Talk dirty",
+    artist: "Jason Derulo",
+    img: "./images/43.jpg",
+    src: "43"
+  },
+  {
+    name: "Taste",
+    artist: "Tyga",
+    img: "./images/44.jpg",
+    src: "44"
+  },
+  {
+    name: "Tkn",
+    artist: "ROSALíA & Travis Scott",
+    img: "./images/45.jpg",
+    src: "45"
+  },
+  {
+    name: "Umbrella (feat. JAY-Z)",
+    artist: "Rihanna",
+    img: "./images/46.jpg",
+    src: "46"
+  },
+  {
+    name: "Under the influence",
+    artist: "Chris Brown",
+    img: "./images/47.jpg",
+    src: "47"
+  },
+  {
+    name: "What it is (solo ver)",
+    artist: "Doechii",
+    img: "./images/48.jpg",
+    src: "48"
+  },
+  {
+    name: "Wait a minute",
+    artist: "Willow Smith",
+    img: "./images/49.jpg",
+    src: "49"
+  },
+  {
+    name: "We can't stop",
+    artist: "Milley Sirus",
+    img: "./images/50.jpg",
+    src: "02"
+  },
+  {
+    name: "We don't talk anymore",
+    artist: "Charlie Puth & Selena Gomez",
+    img: "./images/51.jpg",
+    src: "51"
+  }
+];
+// like this paste it and remember to give comma after ending of this bracket }
+// {
+//   name: "Here is the music name",
+//   artist: "Here is the artist name",
+//   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+// }
